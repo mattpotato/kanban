ec8948ef3383d675963851c64ade2e797214f7ba
matt <38515217+mattpotato@users.noreply.github.com>
2023-10-25 16:02:20 -0700

    fix nullable tasklist position

---
+++ supabase/migrations/20231025230150_list_not_nullable.sql
@@ -0,0 +1,14 @@
+drop policy "Enable update for projects on email" on "public"."project";
+
+alter table "public"."task_list" alter column "position" set not null;
+
+create policy "Enable update for projects based on user id"
+on "public"."project"
+as permissive
+for update
+to public
+using ((auth.uid() = creator_id))
+with check ((auth.uid() = creator_id));
+
+
+

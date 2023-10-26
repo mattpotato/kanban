7bf3ee7dd99158a2d1b095d05e281825e12a1b18
matt <38515217+mattpotato@users.noreply.github.com>
2023-10-26 11:39:00 -0700

    add type to server client

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,9 +1,10 @@
+import { Database } from '@/schema.gen'
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export const createClient = () => {
   const cookieStore = cookies()
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {

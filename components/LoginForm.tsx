4a4ad5b4b94bf42ec3348b1eaccebbe028935870
matt <38515217+mattpotato@users.noreply.github.com>
2023-10-31 22:53:14 -0700

    add tooltip to login

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -2,7 +2,7 @@
 import { useSearchParams } from "next/navigation";
 
 const LoginForm = () => {
-  const searchParams = useSearchParams(); 
+  const searchParams = useSearchParams();
   const error = searchParams.get("error");
   const message = searchParams.get("message");
 
@@ -35,19 +35,24 @@ const LoginForm = () => {
         required
       />
       <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2">
-        Sign In
+        Sign in
       </button>
       <div className="flex items-center">
         <div className="flex-1 h-px bg-gray-300 opacity-50 m-4" />
-        OR
+        No account?
         <div className="flex-1 h-px bg-gray-300 opacity-50 m-4" />
       </div>
-      <button
-        formAction="/auth/sign-up"
-        className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-      >
-        Sign up with email
-      </button>
+      <div className="flex relative group">
+        <button
+          formAction="/auth/sign-up"
+          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2 flex-1"
+        >
+          Sign up with email
+        </button>
+        <div className="hidden group-hover:block absolute top-12 z-50 bg-gray-200 p-2 w-full">
+          You can sign up with any email for testing/demo purposes. No verification needed.
+        </div>
+      </div>
       {message && <div className="border border-gray-400 p-3 rounded bg-gray-100">{message}</div>}
       {error && <div className="border border-red-500 p-3 rounded bg-red-50">{error}</div>}
     </form>
